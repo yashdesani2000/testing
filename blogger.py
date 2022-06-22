6b8fa6ca41c9cb6a9f4bde7c42b64d0369b0e787
yashdesani2000 <105808128+yashdesani2000@users.noreply.github.com>
2022-06-22 16:19:42 +0530

    Update blogger.py

--- blogger.py
+++ blogger.py
@@ -18,15 +18,15 @@ for ip in proxies:
     list = ['socks4://'+ip, 'socks5://'+ip]
     for i in list:
         try:
-            res = s.get(url, headers=header, proxies={'https': i, 'http': i}, timeout=5)
+            res = s.get(url, headers=header, proxies={'https': i, 'http': i})
             # print(res.text)
             soup = BeautifulSoup(res.text, 'lxml')
             # print(soup)
             hi = soup.find('div', class_='post-body entry-content')
             # print(ip, 'working')
         except:
-            pass
+            print("n")
 open('yash.txt').close()
 open('user_agents.txt').close()
 api = "https://api.telegram.org/bot5317123296:AAE7U6qJdnkhL19_AZfKiDWuq-752j0VGe8/sendmessage?chat_id=-1001677829704&text=Finished"
-b = requests.get(api)
+#b = requests.get(api)
