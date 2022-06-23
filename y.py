c922ff5c95834e3955b32f4849d6676ea8087d53
yashdesani2000 <105808128+yashdesani2000@users.noreply.github.com>
2022-06-23 21:06:39 +0530

    Add files via upload

---
+++ y.py
@@ -0,0 +1,36 @@
+import requests
+import random
+from list import user_agents
+
+# f = open('user_agent.txt').read().splitlines()
+print("Running")
+proxy = []
+# r = random.choice(user_agents)
+# print(r)
+
+def socks5():   
+        url = 'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http'
+        s = requests.Session()
+        res = s.get(url).text.splitlines()
+        for i in res:
+                proxy.append(i)  
+        for p in range(30):
+                user_agent = random.choice(user_agents)
+                url2 = 'https://www.yashdesani.tk/2022/06/hi-yash.html'
+                header = { 'user-agent': user_agent}
+                s = requests.Session()
+                ip = random.choice(proxy)
+            
+                if user_agent != None:
+                        try:
+                            s.get(url=url2, headers=header, proxies={'https': ip, 'http': ip}, timeout=5)
+                            #print(p, i, 'working')
+                        except:
+                                        pass
+                                       
+
+while True:
+        proxy.clear()
+        socks5()
+ 
+
